13fca121d554fd2f772c22a449798455876cb6d8
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-05-27 18:07:00 +0600

    add login sign up page

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -1,5 +1,26 @@
+import "./css/login-signup.css";
+
 function LoginSignup() {
-  return <div>LoginSignup</div>;
+  return (
+    <div className="login-signup">
+      <div className="login-signup-container">
+        <h1>Sign Up</h1>
+        <div className="login-signup-fields">
+          <input type="text" placeholder="Your name" />
+          <input type="email" placeholder="Your email" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="login-signup-login">
+          Already have an account? <span>Login here</span>
+        </p>
+        <div className="login-signup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing I agree to the terms of use & privacy policy</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginSignup;
