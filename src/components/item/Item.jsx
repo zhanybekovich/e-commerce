e579ab4f509abadaa4d9ab6a71726390385d69a5
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-05-27 21:11:47 +0600

    add link

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
 import "./item.css";
 function Item(props) {
   return (
     <div className="item">
-      <img src={props.image} alt="" />
+      <Link to={`/product/${props.id}`}>
+        <img src={props.image} alt="" />
+      </Link>
+
       <p>{props.name}</p>
       <div className="item-prices">
         <div className="item-price-new">${props.new_price}</div>
