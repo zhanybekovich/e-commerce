f2df5046c00455c99d140068466f73ca49c8d8f4
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-05-27 21:11:57 +0600

    add product single page

---
+++ src/components/product-display/ProductDisplay.jsx
@@ -0,0 +1,69 @@
+import "./product-display.css";
+import starIcon from "../assets/star_icon.png";
+import starDullIcon from "../assets/star_dull_icon.png";
+
+function ProductDisplay(props) {
+  const { product } = props;
+
+  return (
+    <div className="product-display">
+      <div className="product-display-left">
+        <div className="product-display-img-list">
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+        </div>
+        <div className="product-display-image">
+          <img
+            className="product-display-main-img"
+            src={product.image}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="product-display-right">
+        <h1>{product.name}</h1>
+        <div className="product-display-right-stars">
+          <img src={starIcon} alt="" />
+          <img src={starIcon} alt="" />
+          <img src={starIcon} alt="" />
+          <img src={starIcon} alt="" />
+          <img src={starDullIcon} alt="" />
+          <p>(122)</p>
+        </div>
+        <div className="product-display-right-prices">
+          <div className="product-display-right-price-old">
+            ${product.old_price}
+          </div>
+          <div className="product-display-right-price-new">
+            ${product.new_price}
+          </div>
+        </div>
+        <div className="product-display-right-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias fugit
+          eos, asperiores molestiae est perferendis laudantium ipsam cumque.
+        </div>
+        <div className="product-display-right-size">
+          <h1>Select Size</h1>
+          <div className="product-display-right-sizes">
+            <div>Small</div>
+            <div>Medium</div>
+            <div>Large</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+        <button>Add To Cart</button>
+        <p className="product-display-right-category">
+          <span>Category: </span>Women, T-Shirt, CropTop
+        </p>
+        <p className="product-display-right-category">
+          <span>Tags: </span>Modern, Latest
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ProductDisplay;
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,20 @@
+import { useContext } from "react";
+import { ShopContext } from "../context/shopContext";
+import { useParams } from "react-router-dom";
+import Breadcrumbs from "../components/breadcrumbs/Breadcrumbs";
+import ProductDisplay from "../components/product-display/ProductDisplay";
+
 function Product() {
-  return <div>Product</div>;
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
+
+  return (
+    <div>
+      <Breadcrumbs product={product} />
+      <ProductDisplay product={product} />
+    </div>
+  );
 }
 
 export default Product;
