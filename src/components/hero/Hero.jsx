8ed1dc322e487a931daf9a0692375e60493cfe41
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-05-28 18:56:35 +0600

    improve hero structure

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,30 +1,31 @@
 import "./hero.css";
-import handIcon from "../assets/hand_icon.png";
-import arrowIcon from "../assets/arrow.png";
 import heroImage from "../assets/hero_image.png";
+import Container from "../container/Container";
+import { Link } from "react-router-dom";
 
 function Hero() {
   return (
-    <div className="hero">
-      <div className="hero-left">
-        <h2>New Arrivals Only</h2>
-        <div>
-          <div className="hero-hand-icon">
-            <p>new</p>
-            <img src={handIcon} alt="icon" />
+    <section className="hero">
+      <Container>
+        <div className="hero-inner">
+          <div className="hero-left">
+            <h2>New Arrivals Only</h2>
+            <div>
+              <div className="hero-hand-icon">
+                <p>new collections for everyone</p>
+              </div>
+            </div>
+
+            <Link to="/mens" className="hero-latest-btn">
+              Latest Collection
+            </Link>
           </div>
-          <p>collection</p>
-          <p>for everyone</p>
+          <div className="hero-right">
+            <img src={heroImage} alt="" />
+          </div>
         </div>
-        <div className="hero-latest-btn">
-          <div>Latest Collection</div>
-          <img src={arrowIcon} alt="" />
-        </div>
-      </div>
-      <div className="hero-right">
-        <img src={heroImage} alt="" />
-      </div>
-    </div>
+      </Container>
+    </section>
   );
 }
 
